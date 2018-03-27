b4f3002d57593f05c8298877f3f97effed663a5c
mross <mross@nuvi.local>
2018-03-27 12:12:06 -0700

    Committing after third submission

--- week4/src/Board.java
+++ week4/src/Board.java
@@ -1,46 +1,61 @@
+import edu.princeton.cs.algs4.StdOut;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *  Princeton/Coursera Algorithms Part 1
  *  <p />
  *  Week 4 Assignment: "8 Puzzle"
  *  <p />
  *  Solve the 8-puzzle problem (and its natural generalizations) using the A*
- *  search algorithm.  Organize your program by creating an immutable data
+ *  search algorithm.
+ *  <p />
+ *  Organize your program by creating an immutable data
  *  type Board with the following API:
  *  <pre>
  *      public class Board {
  *
- *          // construct a board from an n-by-n array of blocks
- *          // (where blocks[i][j] = block in row i, column j)
+ *          // construct a _values from an n-by-n array of blocks
+ *          // (where blocks[_row][_col] = block in row _row, column _col)
  *          public Board(int[][] blocks)
  *
- *          // board dimension n
+ *          // _values dimension n
  *          public int dimension()
  *
- *          // number of blocks out of place
+ *          // number of blocks out of placebelow)
  *          public int hamming()
  *
  *          // sum of Manhattan distances between blocks and goal
  *          public int manhattan()
  *
- *          // is this board the goal board?
+ *          // is this _values the goal _values?
  *          public boolean isGoal()
  *
- *          // a board that is obtained by exchanging any pair of blocks
+ *          // a _values that is obtained by exchanging any pair of blocks
  *          public Board twin()
  *
- *          // does this board equal y?
+ *          // does this _values equal y?below)
  *          public boolean equals(Object y)
  *
  *          // all neighboring boards
  *          public Iterable<Board> neighbors()
  *
- *          // string representation of this board (in the output format specified below)
+ *          // string representation of this _values (in the output format specified below)
  *          public String toString()
  *
  *          // unit tests (not graded)
  *          public static void main(String[] args)
  *      }
  *  </pre>
+ *  Your program should work correctly for arbitrary n-by-n boards (for
+ *  any 2 ≤ n < 128), even if it is too slow to solve some of them in a
+ *  reasonable amount of time.
+ *  <p />
  *  Corner cases:
  *  <pre>
  *      You may assume that the constructor receives an n-by-n array containing
@@ -52,60 +67,265 @@
  *      Your implementation should support all Board methods in time
  *      proportional to n2 (or better) in the worst case.
  *  </pre>
+ *  Input and Output Formats:
+ *  <pre>
+ *      The input and output format for a board is the board dimension
+ *      n followed by the n-by-n initial board, using 0 to represent the
+ *      blank square. As an example:
+ *
+ *          % more puzzle04.txt
+ *          3
+ *            0  1  3
+ *            4  2  5
+ *            7  8  6
+ *  </pre>_values
  *  @author Marty Ross
  */
 public class Board {
 
-    /** construct a board from an n-by-n array of blocks
-        (where blocks[i][j] = block in row i, column j)
+    /**
+     *  Immutable array to hold the board's values
      */
-    public Board(int[][] blocks) {
+	private final byte[] _values;
+	private final byte[] _valueMap;
+
+    /**
+     *  Size of an edge of the board
+     */
+	private final int _size;
+	private final int _hammingDistance;
+	private final int _manhattanDistance;
+
+	private static final Map<Board, Collection<Board>> NEIGHBORS = new HashMap<>();
+
+
+    /**
+     *  Coord of a position within the board
+     */
+	private class Coord {
+
+	    private final int _row, _col;
+
+	    Coord(final int pos) {
+			_row = pos / _size;
+			_col = pos % _size;
+		}
+
+		Coord(final int row, final int col) {
+			_row = row;
+			_col = col;
+		}
+
+		int getManhattanDistanceTo(final Coord c) {
+			return Math.abs(_row - c._row) + Math.abs(_col - c._col);
+		}
+
+		int getBoardPos() {
+	        return _row * _size + _col;
+        }
+	}
+
+    /**
+     *  @param valueMatrix matrix of board values
+     */
+    public Board(final int[][] valueMatrix) {
+        this(linearizeMatrixIntoArray(valueMatrix));
+    }
+
+    /**
+     *  @param values array of board values
+     */
+    private Board(final byte[] values) {
+
+        _values = values;
+        _size = (int) Math.sqrt(_values.length);
+
+        _valueMap = new byte[_size * _size];    // reverse-lookup map
+        for (int boardPos = 0; boardPos < _size * _size; boardPos++) {
+            _valueMap[_values[boardPos]] = (byte) boardPos;
+        }
+
+        int hammingDistance = 0;
+        for (int pos = 0; pos < _size * _size - 1; pos++) {
+            if (valueAt(pos) != pos + 1) {
+                hammingDistance++;
+            }
+        }
+        _hammingDistance = hammingDistance;
+
+        int manhattanDistance = 0;
+        for (int pos = 0; pos < _size * _size - 1; pos++) {
+            if (valueAt(pos) != pos + 1) {
+                manhattanDistance += new Coord(pos)
+                    .getManhattanDistanceTo(findValue(pos + 1));
+            }
+        }
+        _manhattanDistance = manhattanDistance;
 
     }
 
-    /** board dimension n */
+    /**
+     *  @return size of an edge of the matrix
+     */
     public int dimension() {
-        return -1;
+        return _size;
     }
 
-    /** number of blocks out of place */
+    /**
+     *  @return number of blocks out of place
+     */
     public int hamming() {
-        return -1;
+        return _hammingDistance;
     }
 
-    /** sum of Manhattan distances between blocks and goal */
+    /**
+     *  @return sum of Manhattan (vertical and horizontal) distances
+     *  between blocks to their goal positions, plus the number of
+     *  moves made so far to get to the search node
+     */
     public int manhattan() {
-        return -1;
+        return _manhattanDistance;
     }
 
-    /** is this board the goal board? */
+    /**
+     * @return {@code true} if we've reached the goal
+     */
     public boolean isGoal() {
-        return false;
+        return _hammingDistance == 0;
     }
 
-    /** a board that is obtained by exchanging any pair of blocks */
+    /**
+     *  @return a copy of this board having a pair of blocks exchanged
+     */
     public Board twin() {
-        return null;
+		final int row = findValue(0)._row == 0 ? 1 : 0;
+		return swap(new Coord(row, 0), new Coord(row, 1));
     }
 
-    /** does this board equal y? */
-    public boolean equals(Object y) {
-        return false;
+    /**
+     *  @return {@code true} iff this board equal {@code boardObject}
+     */
+    public boolean equals(final Object boardObject) {
+        if (!(boardObject instanceof Board)) {
+			return false;
+		}
+		final Board board = (Board) boardObject;
+		if (_size != board._size) {
+			return false;
+		}
+		for (int boardPos = 0; boardPos < _size * _size; boardPos++) {
+		    if (_values[boardPos] != board._values[boardPos]) {
+		        return false;
+            }
+        }
+		return true;
     }
 
-    /** all neighboring boards */
+    /**
+     *  @return iterator to possible child boards
+     */
     public Iterable<Board> neighbors() {
-        return null;
+        final Iterable<Board> cachedNeighbors = NEIGHBORS.get(this);
+        if (cachedNeighbors != null) {
+            return cachedNeighbors;
+        }
+
+        final List<Board> calculatedNeighbors = new ArrayList<>(4);
+        final Coord p = findValue(0);
+        if (p._row > 0) {
+            calculatedNeighbors.add(swap(p, new Coord(p._row - 1, p._col)));
+        }
+        if (p._row < _size - 1) {
+            calculatedNeighbors.add(swap(p, new Coord(p._row + 1, p._col)));
+        }
+        if (p._col > 0) {
+            calculatedNeighbors.add(swap(p, new Coord(p._row, p._col - 1)));
+        }
+        if (p._col < _size - 1) {
+            calculatedNeighbors.add(swap(p, new Coord(p._row, p._col + 1)));
+        }
+
+        NEIGHBORS.put(this, calculatedNeighbors);
+        StdOut.printf("cacheSize(%s)\n", NEIGHBORS.size());
+        return calculatedNeighbors;
     }
 
-    /** string representation of this board (in the output format specified below) */
+    /**
+     *  @return string representation of this board's values
+     *  (in the output format specified in this class' Javadoc)
+     */
     public String toString() {
-        return null;
+        final StringBuilder s = new StringBuilder(_size + "\n");
+        for (int row = 0; row < _size; row++) {
+            for (int col = 0; col < _size; col++) {
+                s.append(String.format("%2d ", _values[row * _size + col]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 
-    /** unit tests (not graded) */
-    public static void main(String[] args) {
 
+    //
+    //  Private instance methods
+    //
+
+    /**
+     *  @param value value being sought
+     *  @return coordinate where the value was found within the current board
+     */
+    private Coord findValue(final int value) {
+        return new Coord(_valueMap[value]);
+	}
+
+    /**
+     *  @param boardPos position on the board
+     *  @return value at the specified position
+     */
+	private byte valueAt(final int boardPos) {
+		return _values[boardPos];
+	}
+
+    /**
+     *  @param c coordinate on the board
+     *  @return value at the specified coordinate
+     */
+	private byte valueAt(final Coord c) {
+		return valueAt(c.getBoardPos());
+	}
+
+    /**
+     *  @param c1 coordinate of first value
+     *  @param c2 coordinate of second value
+     *  @return a new board, with values swapped from first & second coordinates
+     */
+	private Board swap(final Coord c1, final Coord c2) {
+		final byte[] newBoard = _values.clone();
+		newBoard[c1.getBoardPos()] = valueAt(c2);
+		newBoard[c2.getBoardPos()] = valueAt(c1);
+		return new Board(newBoard);
+	}
+
+	public static byte[] linearizeMatrixIntoArray(final int[][] valueMatrix) {
+
+	    if (valueMatrix == null) {
+	        throw new IllegalArgumentException();
+        }
+
+        final int size = valueMatrix.length;
+        final byte[] values = new byte[size * size];
+
+        int boardPos = 0;
+        for (final int[] valueMatrixRow : valueMatrix) {
+            if (valueMatrixRow == null || valueMatrixRow.length != size) {
+                throw new IllegalArgumentException();
+            }
+            for (int col = 0; col < size; col++) {
+                values[boardPos++] = (byte) valueMatrixRow[col];
+            }
+        }
+
+        return values;
     }
 
 }
--- week4/src/Solver.java
+++ week4/src/Solver.java
@@ -1,5 +1,10 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
+
+import edu.princeton.cs.algs4.Stopwatch;
+import java.util.Comparator;
 
 /**
  *  Princeton/Coursera Algorithms Part 1
@@ -38,38 +43,80 @@ import edu.princeton.cs.algs4.StdOut;
  */
 public class Solver {
 
-    /**
-     *  Find a solution to the initial board (using the A* algorithm)
-     */
-    public Solver(Board initial) {
+    private Node solution;
+
+    private static class Node {
+        private final Board board;
+        private final Node prev;
+        private final int moves;
+        Node(Board board, Node prev, int moves) {
+            this.board = board;
+            this.prev = prev;
+            this.moves = moves;
+        }
 
     }
 
-	/**
+    /**
+     *  Find a solution to the initial board (using the A* algorithm)
+     */
+    public Solver(final Board initial) {
+        final Comparator<Node> manhattanComparator = (
+            (lhsNode, rhsNode) -> {
+                final int lhsNodeCost = getCost(lhsNode);
+                final int rhsNodeCost = getCost(rhsNode);
+                if (lhsNodeCost == rhsNodeCost) {
+                    return lhsNode.board.hamming() - rhsNode.board.hamming();
+                }
+                return lhsNodeCost - rhsNodeCost;
+            }
+        );
+        solveWith(initial, manhattanComparator);
+    }
+
+    /**
      *  Is the initial board solvable?
      */
     public boolean isSolvable() {
-        return false;
+        return solution != null;
     }
 
     /**
      *  Min number of moves to solve initial board; -1 if unsolvable
      */
     public int moves() {
-        return -1;
+        return solution == null ? -1 : solution.moves;
     }
 
-	/**
-     * Sequence of boards in a shortest solution; null if unsolvable
+    /**
+     *  @return sequence of boards in a shortest solution,
+     *  or {@code null} if unsolvable
      */
     public Iterable<Board> solution() {
-        return null;
+        if (solution == null) {
+            return null;
+        }
+        final Stack<Board> boardStack = new Stack<>();
+        for (Node curNode = solution; curNode != null; curNode = curNode.prev) {
+            boardStack.push(curNode.board);
+        }
+        return boardStack;
     }
 
-	/**
-     * solve a slider puzzle (given below)
+    /**
+     *  Test Client
+     *  </p >
+     *  <pre>
+     *      Solver test client. Use the following test client to read a puzzle
+     *      from a file (specified as a command-line argument) and print the
+     *      solution to standard output.
+     *  </pre>
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+
+        if (args == null || args.length < 1) {
+            throw new IllegalArgumentException("no filename(s) specified");
+        }
 
         // create initial board from file
         final In in = new In(args[0]);
@@ -81,19 +128,81 @@ public class Solver {
             }
         }
 
-        final Board initial = new Board(blocks);
-
-        // solve the puzzle
-        final Solver solver = new Solver(initial);
+        final Stopwatch stopwatch = new Stopwatch();
+        final Solver solver = new Solver(new Board(blocks));
+        StdOut.printf("solver returned in %.2fs\n", stopwatch.elapsedTime());
 
         // print solution to standard output
         if (!solver.isSolvable()) {
             StdOut.println("No solution possible");
+            return;
         }
-        else {
-            StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
-                StdOut.println(board);
+
+        StdOut.printf("Minimum number of moves = %s\n", solver.moves());
+        for (final Board board : solver.solution()) {
+            StdOut.println(board);
         }
     }
+
+    private int getCost(final Node node) {
+        return node.moves + node.board.manhattan();
+    }
+
+    private void solveWith(Board initial, Comparator<Node> comparator) {
+
+        final MinPQ<Node> mainPq = new MinPQ<>(comparator);
+        mainPq.insert(new Node(initial, null, 0));
+
+        final MinPQ<Node> twinPq = new MinPQ<>(comparator);
+        twinPq.insert(new Node(initial.twin(), null, 0));
+
+        while(true) {
+
+            assert !mainPq.isEmpty() && !twinPq.isEmpty();
+
+//            printProgress(pqInitial);
+
+            solution = walkTowardSolution(mainPq);
+            if (solution != null) {
+                break;
+            }
+
+            if (walkTowardSolution(twinPq) != null) {
+                StdOut.println("unsolveable");
+                solution = null;
+                break;
+            }
+
+        }
+
+    }
+
+    private Node walkTowardSolution(final MinPQ<Node> minPq) {
+
+        assert !minPq.isEmpty();
+
+        final Node curNode = minPq.delMin();
+        if (curNode.board.isGoal()) {
+            return curNode;
+        }
+
+        for (final Board n : curNode.board.neighbors()) {
+            if (curNode.prev == null || !n.equals(curNode.prev.board)) {
+                minPq.insert(new Node(n, curNode, curNode.moves + 1));
+            }
+        }
+        return null;
+    }
+
+//    private void printProgress(MinPQ<Node> pqInitial) {
+//        StdOut.println("pass");
+//        for (
+//            final Iterator<Node> nodeIterator = pqInitial.iterator();
+//            nodeIterator.hasNext();
+//        ) {
+//            final Node node = nodeIterator.next();
+//            StdOut.printf("node(%s)\n", node.board);
+//        }
+//    }
+
 }
